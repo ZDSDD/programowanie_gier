9b74300f09dc18a68a96b5cec354181f75307539
ZDSDD <sebasadowy@gmail.com>
2024-05-13 11:22:07 +0200

    add collisions

--- Game/Game.h
+++ Game/Game.h
@@ -7,6 +7,7 @@
 
 
 #include "GameLevel.h"
+#include "BallObject.h"
 
 enum GameState {
     GAME_ACTIVE,
@@ -14,6 +15,16 @@ enum GameState {
     GAME_WIN
 };
 
+// Represents the four possible (collision) directions
+enum Direction {
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
+};
+// Defines a Collision typedef that represents collision data
+typedef std::tuple<bool, Direction, glm::vec2> Collision; // <collision?, what direction?, difference vector center - closest point>
+typedef std::tuple<bool, Direction, glm::vec2> Collision; // <collision?, what direction?, difference vector center - closest point>
 // Initial size of the player paddle
 const glm::vec2 PLAYER_SIZE(100.0f, 20.0f);
 // Initial velocity of the player paddle
@@ -22,24 +33,37 @@ const float PLAYER_VELOCITY(500.0f);
 // Game holds all game-related state and functionality.
 // Combines all game-related data into a single class for
 // easy access to each of the components and manageability.
-class Game
-{
+class Game {
 public:
     // game state
-    GameState               State;
-    bool                    Keys[1024];
-    unsigned int            Width, Height;
-    std::vector<GameLevel>  Levels;
-    unsigned int            Level;
+    GameState State;
+    bool Keys[1024];
+    unsigned int Width, Height;
+    std::vector<GameLevel> Levels;
+    unsigned int Level;
+
+
+
     // constructor/destructor
     Game(unsigned int width, unsigned int height);
+
     ~Game();
+
     // initialize game state (load all shaders/textures/levels)
     void Init();
+
     // game loop
     void ProcessInput(float dt);
+
     void Update(float dt);
+
     void Render();
+
+    void DoCollisions();
+
+    void ResetLevel();
+
+    void ResetPlayer();
 };
 
 
